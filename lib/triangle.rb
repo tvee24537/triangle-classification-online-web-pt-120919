cd4592fce31a57286439a6f42f51f65bfd09db96
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-25 02:58:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def kind
     if valid? == false
       raise TriangleError
-    if side_1 == side_2 && side_2 == side_3
+    elsif side_1 == side_2 && side_2 == side_3
       return :equilateral
     elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
       return :isosceles
