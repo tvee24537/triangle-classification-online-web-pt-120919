c4735a63a51977ff7efd07e5a155432893aacb13
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-25 02:37:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,23 +8,20 @@ class Triangle
   
   def kind
     valid?
-      if side_1 == side_2 && side_2 == side_3
-        return :equilateral
-      elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
-        return :isosceles
-      else
-        return :scalene
-      end
+    if side_1 == side_2 && side_2 == side_3
+      return :equilateral
+    elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+      return :isosceles
     else
-      raise TriangleError
+      return :scalene
     end
   end
  
   def valid?
     valid_triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)]
     
-    [side_1, side_2, side_3].each { |length| real_triangle << false if length <= 0 }
-    raise TriangleError if real_triangle.include?(false)
+    [side_1, side_2, side_3].each { |length| valid_triangle << false if length <= 0 }
+    raise TriangleError if valid_triangle.include?(false)
   end
 end 
 
