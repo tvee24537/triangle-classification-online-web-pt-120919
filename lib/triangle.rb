f0650cad4989f3dedaa1fee15995fb5f7a72fe9a
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-25 02:56:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,9 +13,9 @@ class Triangle
   def kind
     if (negative? == true || valid? == false)
       raise TriangleError 
-    elsif (@side_1 == @side_2) && (@side_1 == @side_3)
+    elsif (side_1 == side_2) && (side_1 == side_3)
       return :equilateral
-    elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+    elsif (side_2 == side_3) || (side_1 == side_2) || (side_1 = side_3)
       return :isosceles
     else
       return :scalene
@@ -23,7 +23,7 @@ class Triangle
   end
  
   def valid?
-    (@side_1 + @side_2 > @side_3) && (@side_2 + @side_3 > @side_1) && (@side_1 + @side_3 > @side_2)
+    (side_1 + side_2 > side_3) && (side_2 + side_3 > side_1) && (side_1 + side_3 > side_2)
   end  
     
   def negative?
