9c46e821eb59307e4122a2444c5eda7b55e52229
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-25 02:28:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,9 @@
 class Triangle
   # write code here
   def initialize(side_1, side_2, side_3)
-    @side_length = []
-    @side_length << side_1
-    @side_length << side_2
-    @side_length << side_3
+    @side_1 << side_1
+    @side_2 << side_2
+    @side_3 << side_3
   end
   
   def kind
