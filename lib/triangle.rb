f579641aa85c602fa8920077d675b7b1ba28535d
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-25 02:22:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,20 +6,6 @@ class Triangle
     @side_length << side_2
     @side_length << side_3
   end
-  
-  def valid?
-    sum_one_two = @side_length[0] + @side_length[1]
-    sum_one_three = @side_length[0] + @side_length[2]
-    sum_two_three = @side_length[1] + @side_length[2]
-
-    if (@side_length.none? {|side| side <= 0}) &&
-      (sum_one_two > @side_length[2] && sum_one_three > @side_length[1] && sum_two_three > @side_length[0])
-      return true
-    else
-      return false
-    end
-  end
-
   def kind
     if valid?
       if @side_length.uniq.length == 1
@@ -33,7 +19,21 @@ class Triangle
       raise TriangleError
     end
   end
-end
+end  
+  def valid?
+    sum_one_two = @side_length[0] + @side_length[1]
+    sum_one_three = @side_length[0] + @side_length[2]
+    sum_two_three = @side_length[1] + @side_length[2]
+
+    if (@side_length.none? {|side| side <= 0}) &&
+      (sum_one_two > @side_length[2] && sum_one_three > @side_length[1] && sum_two_three > @side_length[0])
+      return true
+    else
+      return false
+    end
+  end
+
+
 
 class TriangleError < StandardError
 
