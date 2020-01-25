7e34054c5d637027cc8f1fa5be3d2ffdc7118c99
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-25 02:37:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   end
   
   def kind
-    if valid?
+    valid?
       if side_1 == side_2 && side_2 == side_3
         return :equilateral
       elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
