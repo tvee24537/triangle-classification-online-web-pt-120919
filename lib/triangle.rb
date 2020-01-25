0a265aea0e34c97e5422ab96f578ba6086b53235
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-25 02:35:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,11 +21,9 @@ class Triangle
   end
  
   def valid?
+    valid_triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)]
     
-      return true
-    else
-      return false
-    end
+    if [side_1, side_2, side_3].each {|length| valid_triangle}
   end
 end 
 
