ef0736d42b393fd81f449015aa34b38452c1d902
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-25 03:07:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,10 +2,13 @@ class Triangle
   # write code here
   attr_reader :side_1, :side_2, :side_3
   
+  @@sides = []
+  
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
+    @@sides = [side_1, side_2, side_3]
   end
   
   def kind
@@ -21,9 +24,15 @@ class Triangle
   end
  
   def valid?
-    valid_triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)]
+#    valid_triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)]
     
-    [side_1, side_2, side_3].each { |length| valid_triangle << false if length <= 0 }
+    if (@@sides.none? {|length| length <= 0}) && ((side_1 + side_2 > side_3) && (side_1 + side_3 > side_2) && (side_2 + side_3 > side_1))
+      return true
+    else 
+      return false
+    end
+      
+#    [side_1, side_2, side_3].each { |length| valid_triangle << false if length <= 0 }
   end
 end 
 
